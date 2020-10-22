9b4c32a2dfd5ed48c49395190552ad54dfc270de
KAVANABELAVADI <50482786+KAVANABELAVADI@users.noreply.github.com>
2020-10-22 08:24:23 +0530

    Percentage of singlet combination

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -1,8 +1,29 @@
 #!/bin/bash -x
 
-if [ $((RANDOM % 2)) == 0 ]
-	then
-       		echo "Heads"
-   	else
-       		echo "Tails"
-fi
+declare -A singlet
+
+read -p "Enter Number of Trails - " trails
+temp=$trails
+
+while [ $temp -gt 0 ]
+do
+	res=$((RANDOM % 2))
+        if [ $res == 0 ]
+        then
+                (( heads++ ))
+		singlet[heads]+=" $res"
+        else
+                (( tails++ ))
+		singlet[tails]+=" $res"
+        fi
+        ((temp--))
+done
+echo ${singlet[heads]}
+echo ${singlet[tails]}
+
+echo "Number of heads - "$heads
+echo $heads $trails | awk '{print "Persentage of heads - " ((100*$1/$2)) }'
+echo "Number of tails - "$tails
+echo $tails $trails | awk '{print "Persentage of tails - " ((100*$1/$2)) }'
+
+
