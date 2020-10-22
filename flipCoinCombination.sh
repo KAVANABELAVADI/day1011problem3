101fd70342ae4830f585689172061adcf0f888d3
KAVANABELAVADI <50482786+KAVANABELAVADI@users.noreply.github.com>
2020-10-22 13:05:26 +0530

    Doublet Combination

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -1,29 +1,46 @@
-#!/bin/bash -x
+#!/bin/bash
 
 declare -A singlet
+declare -A doublet
+
+doubletHead=0;
+doubletHeadTail=0;
+doubletTailHead=0;
+doubletTail=0;
 
 read -p "Enter Number of Trails - " trails
 temp=$trails
 
+
 while [ $temp -gt 0 ]
 do
+	#singlet
 	res=$((RANDOM % 2))
-        if [ $res == 0 ]
-        then
-                (( heads++ ))
-		singlet[heads]+=" $res"
-        else
-                (( tails++ ))
-		singlet[tails]+=" $res"
-        fi
+
+	#Doublet
+	res1=$res$((RANDOM%2))
+	case $res1 in
+		00) ((doubletHead++))
+		  	doublet[heads]+=" $res1" ;;
+		01) ((doubletHeadTail++))
+			doublet[headtail]+=" $res1" ;;
+		10) ((doubletTailHead++))
+			doublet[tailhead]+=" $res1" ;;
+		11) ((doubletTail++))
+			doublet[tails]+=" $res1" ;;
+	esac
+
         ((temp--))
 done
-echo ${singlet[heads]}
-echo ${singlet[tails]}
 
-echo "Number of heads - "$heads
-echo $heads $trails | awk '{print "Persentage of heads - " ((100*$1/$2)) }'
-echo "Number of tails - "$tails
-echo $tails $trails | awk '{print "Persentage of tails - " ((100*$1/$2)) }'
+echo ${doublet[heads]}
+echo ${doublet[headtail]}
+echo ${doublet[tailhead]}
+echo ${doublet[tails]}
+
+echo $doubletHead $trails | awk '{print "Persentage of heads combination - " ((100*$1/$2)) }'
+echo $doubletHeadTail $trails | awk '{print "Persentage of head tail combination - " ((100*$1/$2)) }'
+echo $doubletTailHead $trails | awk '{print "Persentage of tail head - " ((100*$1/$2)) }'
+echo $doubletTail $trails | awk '{print "Persentage of tails - " ((100*$1/$2)) }'
 
 
